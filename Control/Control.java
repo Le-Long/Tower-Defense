94ef0db5951c5f61f79b7c308cbc0fa07999ab6b
huuduy16 <44548159+huuduy16@users.noreply.github.com>
2019-11-13 14:01:45 +0700

    new

--- Control/Control.java
+++ Control/Control.java
@@ -6,32 +6,35 @@
 package Control;
 
 /**
- *
  * @author TA
  */
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class Control extends MouseAdapter{
-	
-	private int mouseX;
-	private int mouseY;
-	
-	public void mousePressed(MouseEvent e) {
-	mouseX = e.getX();
-	mouseY = e.getY();              	
-	}
-	
-	public int getMouseX(){
-		return mouseX;
-	}
-	public int getMouseY(){
-		return mouseY;
-	}
-	public void setMouseX(int x){
-		mouseX = x;
-	}
-	public void setMouseY(int y){
-		mouseY = y;
-	}
+public class Control extends MouseAdapter {
+
+    private int mouseX;
+    private int mouseY;
+
+    public void mousePressed(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+
+    public int getMouseX() {
+        return mouseX;
+    }
+
+    public void setMouseX(int x) {
+        mouseX = x;
+    }
+
+    public int getMouseY() {
+        return mouseY;
+    }
+
+    public void setMouseY(int y) {
+        mouseY = y;
+    }
 }
--- Control/Shop.java
+++ Control/Shop.java
@@ -5,37 +5,39 @@
  */
 package Control;
 
-import java.awt.Graphics;
-import java.awt.image.*;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import GameTile.MachinegunTower;
+import GameTile.NormalTower;
+import GameTile.SniperTower;
+import GameTile.Tower;
 
-import GameTile.*;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 /**
- *
  * @author TA
  */
 public class Shop {
-        private int itemCount;
-	private BufferedImage[] itemImage;
-	private Tower towerToPlace;
-	private boolean towerBought = false;
-	private String backgroundImageBuffer;
-	private BufferedImage backgroundImage; 
-	
-	public Shop()
-	{
-		backgroundImageBuffer = "/images/shopBackground.jpg";
-		try {
-			backgroundImage = ImageIO.read(getClass().getResourceAsStream(backgroundImageBuffer));
-		}	catch(IOException exc) {
-				exc.printStackTrace();
-		}
-		itemCount = 8;
-		setShopItems();
-	}
-	public void buyTower(int mouseX, int mouseY, int playerGold)
-	{
+    private int itemCount;
+    private BufferedImage[] itemImage;
+    private Tower towerToPlace;
+    private boolean towerBought = false;
+    private String backgroundImageBuffer;
+    private BufferedImage backgroundImage;
+
+    public Shop() {
+        backgroundImageBuffer = "/images/shopBackground.jpg";
+        try {
+            backgroundImage = ImageIO.read(getClass().getResourceAsStream(backgroundImageBuffer));
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+        itemCount = 8;
+        setShopItems();
+    }
+
+    public void buyTower(int mouseX, int mouseY, int playerGold) {
                 /*Long's coding part
 		if((mouseX > 0 && mouseX<60) && (mouseY < 636 && mouseY > 576))
 		{
@@ -102,96 +104,94 @@ public class Shop {
 				return;
 			}
 		}
-            else */ if((mouseX>60&&mouseX<120) && (mouseY < 696 && mouseY > 636))
-		{
-			towerToPlace = new NormalTower();
-			if(playerGold >= towerToPlace.getCost()){
-				towerBought = true;
-				return;
-			}
-			else{
-				towerToPlace = null;
-				towerBought = false;
-				return;
-			}
-		}
-		else if((mouseX>120&&mouseX<180) && (mouseY < 696 && mouseY > 636))
-		{
-			towerToPlace = new SniperTower();
-			if(playerGold >= towerToPlace.getCost()){
-				towerBought = true;
-				return;
-			}
-			else{
-				towerToPlace = null;
-				towerBought = false;
-				return;
-			}
-		}
-		else if((mouseX>180&&mouseX<240) && (mouseY < 696 && mouseY > 636))
-		{
-			towerToPlace = new MachinegunTower();
-			if(playerGold >= towerToPlace.getCost()){
-				towerBought = true;
-				return;
-			}
-			else{
-				towerToPlace = null;
-				towerBought = false;
-				return;
-			}
-		}
-	}
-	
-	public void setShopItems()
-	{
-                /*Long's coding part*/
-		itemImage = new BufferedImage[itemCount];
-		try {
-			itemImage[0] = ImageIO.read(getClass().getResourceAsStream("/images/towerDefense_NormalTower.png"));
-		}	catch(IOException exc) {
-				exc.printStackTrace();
-		}
-		try {
-			itemImage[1] = ImageIO.read(getClass().getResourceAsStream("/images/towerDefense_SniperTower.png"));
-		}	catch(IOException exc) {
-				exc.printStackTrace();
-		}
-		try {
-			itemImage[2] = ImageIO.read(getClass().getResourceAsStream("/images/towerDefense_MachinegunTower.png"));
-		}	catch(IOException exc) {
-				exc.printStackTrace();
-		}
-	}
-	//GETTERS
-	public int getItemCount(){
-		return itemCount;
-	}
-	public boolean getTowerBought(){
-		return towerBought;
-	}
-	public Tower getTowerToPlace(){
-		return towerToPlace;
-	}
-	//SETTERS
-	public void setTowerBought(boolean inp){
-		towerBought = inp;
-	}
-	public void setTowerToPlace(Tower inp){
-		towerToPlace = inp;
-	}
-	public void draw(Graphics g){
-		g.drawImage(backgroundImage,0,576,null);
-		//g.drawImage(itemImage[0],0,576,null);
-		//g.drawImage(itemImage[1],60,576,null);
-		//g.drawImage(itemImage[1],120,576,null);
-		//g.drawImage(itemImage[3],180,576,null);
-		//g.drawImage(itemImage[3],0,636,null);
-		g.drawImage(itemImage[0],60,636,null);
-		g.drawImage(itemImage[1],120,636,null);
-		g.drawImage(itemImage[2],180,636,null);
-                g.drawString("200",80, 636);
-                g.drawString("400",140, 636);
-                g.drawString("500",200, 636);
-	}
+            else */
+        if ((mouseX > 60 && mouseX < 120) && (mouseY < 696 && mouseY > 636)) {
+            towerToPlace = new NormalTower();
+            if (playerGold >= towerToPlace.getCost()) {
+                towerBought = true;
+                return;
+            } else {
+                towerToPlace = null;
+                towerBought = false;
+                return;
+            }
+        } else if ((mouseX > 120 && mouseX < 180) && (mouseY < 696 && mouseY > 636)) {
+            towerToPlace = new SniperTower();
+            if (playerGold >= towerToPlace.getCost()) {
+                towerBought = true;
+                return;
+            } else {
+                towerToPlace = null;
+                towerBought = false;
+                return;
+            }
+        } else if ((mouseX > 180 && mouseX < 240) && (mouseY < 696 && mouseY > 636)) {
+            towerToPlace = new MachinegunTower();
+            if (playerGold >= towerToPlace.getCost()) {
+                towerBought = true;
+                return;
+            } else {
+                towerToPlace = null;
+                towerBought = false;
+                return;
+            }
+        }
+    }
+
+    public void setShopItems() {
+        /*Long's coding part*/
+        itemImage = new BufferedImage[itemCount];
+        try {
+            itemImage[0] = ImageIO.read(getClass().getResourceAsStream("/images/towerDefense_NormalTower.png"));
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+        try {
+            itemImage[1] = ImageIO.read(getClass().getResourceAsStream("/images/towerDefense_SniperTower.png"));
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+        try {
+            itemImage[2] = ImageIO.read(getClass().getResourceAsStream("/images/towerDefense_MachinegunTower.png"));
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    //GETTERS
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public boolean getTowerBought() {
+        return towerBought;
+    }
+
+    //SETTERS
+    public void setTowerBought(boolean inp) {
+        towerBought = inp;
+    }
+
+    public Tower getTowerToPlace() {
+        return towerToPlace;
+    }
+
+    public void setTowerToPlace(Tower inp) {
+        towerToPlace = inp;
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(backgroundImage, 0, 576, null);
+        //g.drawImage(itemImage[0],0,576,null);
+        //g.drawImage(itemImage[1],60,576,null);
+        //g.drawImage(itemImage[1],120,576,null);
+        //g.drawImage(itemImage[3],180,576,null);
+        //g.drawImage(itemImage[3],0,636,null);
+        g.drawImage(itemImage[0], 60, 636, null);
+        g.drawImage(itemImage[1], 120, 636, null);
+        g.drawImage(itemImage[2], 180, 636, null);
+        g.drawString("200", 80, 636);
+        g.drawString("400", 140, 636);
+        g.drawString("500", 200, 636);
+    }
 }
