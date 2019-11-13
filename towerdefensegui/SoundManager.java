24a354d4e4b6235a406c0a2cd522bc7f1fefe563
huuduy16 <44548159+huuduy16@users.noreply.github.com>
2019-11-13 14:03:30 +0700

    Add files via upload

--- towerdefensegui/SoundManager.java
+++ towerdefensegui/SoundManager.java
@@ -5,32 +5,33 @@
  */
 package towerdefensegui;
 
-import java.io.InputStream;
-import sun.audio.*;
+//import sun.audio.*;
 
 /**
- *
  * @author TA
  */
 public class SoundManager {
-        public InputStream backgroundMusicBuffer;
-        public AudioStream backGroundMusic;
-        public SoundManager(){
-            setBackgroundMusic();
-            // stopBackgroundMusic();
+    /*
+    public InputStream backgroundMusicBuffer;
+    public AudioStream backGroundMusic;
+
+    public SoundManager() {
+        setBackgroundMusic();
+        // stopBackgroundMusic();
+    }
+
+    public void setBackgroundMusic() {
+        try {
+            backgroundMusicBuffer = getClass().getResourceAsStream("/background.wav");
+            backGroundMusic = new AudioStream(backgroundMusicBuffer);
+            AudioPlayer.player.start(backGroundMusic);
+        } catch (Exception exc) {
+            exc.printStackTrace();
         }
-  
-	public void setBackgroundMusic()
-	{
-		try {
-			backgroundMusicBuffer = getClass().getResourceAsStream("/background.wav");
-			backGroundMusic = new AudioStream(backgroundMusicBuffer);
-			AudioPlayer.player.start(backGroundMusic);
-		}	catch(Exception exc) {
-				exc.printStackTrace();
-		}
-	}
-	public void stopBackgroundMusic(){
-		AudioPlayer.player.stop(backGroundMusic);
-	}
+    }
+
+    public void stopBackgroundMusic() {
+        AudioPlayer.player.stop(backGroundMusic);
+    }
+    */
 }
