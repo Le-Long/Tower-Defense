e09e0d554de936e34e5b5dda1c544aaa0d18ee4e
NXT <xuantruongd9@gmail.com>
2019-12-11 22:21:52 +0700

    fix

--- towerdefensegui/HighScore.java
+++ towerdefensegui/HighScore.java
@@ -5,10 +5,15 @@
  */
 package towerdefensegui;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.*;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
@@ -16,5 +21,64 @@ import javax.imageio.ImageIO;
  * @author TA
  */
 public class HighScore extends javax.swing.JPanel {
+    BufferedImage backgroundImage;
+    String imageBuffer = "/images/Highscore.png";
 
+    private String savePath;
+    private String filename = "Highscore.txt";
+    private Font scoreFont = new Font("TimesRoman", Font.PLAIN, 48);
+    private List<Integer> scoreList = new ArrayList<Integer>();
+
+    public HighScore(){
+       // this.setPreferredSize(new Dimension(832, 776));
+        try {
+            backgroundImage = ImageIO.read(getClass().getResourceAsStream(imageBuffer));
+         //   savePath = "/towerdefensegui/";
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+        loadHighscore();
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, this);
+        g.setColor(Color.RED);
+        g.setFont(scoreFont);
+        for (int i = 0; i < scoreList.size(); i++)
+            g.drawString(""+(i+1)+ '.'+scoreList.get(i), 250, 160 + i * 50);
+    }
+
+    private void CreateSave(){
+        try{
+            File file = new File(filename);
+            file.createNewFile();
+            FileWriter fr = null;
+            fr = new FileWriter(file);
+            fr.write(""+0);
+            fr.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void loadHighscore(){
+        try{
+            File f = new File(filename);
+            if (!f.isFile()) CreateSave();
+            Scanner sc = new Scanner(f);
+            while (sc.hasNext() ) {
+                if (sc.hasNextInt()) {
+                    int x = sc.nextInt();
+                    scoreList.add(x);
+                }
+            }
+            sc.close();
+            Collections.sort(scoreList);
+            Collections.reverse(scoreList);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
